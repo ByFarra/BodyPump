ccbf4a34d1ac7d65bd148e30821558ddd5c7556b
ByFarra <f.kobulov@mail.ru>
2024-10-11 13:58:07 +0500

    Add files via upload

---
+++ JavaScript/scrypt.js
@@ -0,0 +1,168 @@
+"use strict";
+// let a = 10;
+//alert(a);
+//a = 20;
+//alert(a);
+//let a = 1 + 2 + 3;
+//alert(a);
+/*
+let a = 10;
+let b = 2;
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+*/
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+// alert(result);
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// alert(a+b+c);
+// let a = 10;
+// let b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+// let a = 1.5;
+// let b = 0.75;
+// let result = a + b;
+// alert(result);
+// let a = -100;
+// alert(a);
+// let a = 52;
+// let b = -a;
+// alert(b);
+// let a = 13;
+// let b = 5;
+// alert(a % b);
+// let a = 2;
+// alert(a**10);
+// let str = 'ASKEROVYUH';
+// alert(str);
+// let str = "hmstrusdt!!!";
+// alert(str);
+// let str = "java";
+// let str1 = "script";
+// alert(str +' '+  str1);
+// let hmstr = 'kombat criminal';
+// alert(hmstr.length);
+// let str1 = `xxx`;
+// let str2 = `yyy`;
+// let txt  = `aaa  ${str1}  bbb ${str2} ccc`;
+// alert(txt);
+// let str = `a
+// b
+// c`;
+// alert(str);
+// let a;
+// alert(a)
+// let a = null
+// alert(a)
+// let a = true
+// let b = false
+// alert(a)
+// alert(b)
+// let a = "chupik";
+// let b = "sosat";
+// alert(a * b);
+// alert( -10 / 0);
+// let a = 5;
+// let b = 6;
+// let c = 10;
+// console.log(a, b, c);
+// console.log(123);
+// console.log("123");
+// alert(a);
+// const PI = 3.14;
+// let r = 3;
+// let c = 2*PI*r;
+// alert(c)
+// let a = (-'5') + (-'2');
+// alert(a);
+// let a = "10";
+// let b = "20";
+// alert(Number(a) + Number(b));
+// let a = 2;
+// let b = 3;
+// alert(a + b)
+// let num = parseFloat("5.5px");
+// let num2 = parseFloat("6.25px");
+// alert(num + num2 + "px");
+// let num1 = 1;
+// let num2 = 2;
+// alert(String(num1) + String(num2));
+// let a = 123456789;
+// alert(String(a).length);
+// let a = 123456;
+// let b = 789;
+// alert(String(a).length + String(b).length)
+// let a = 'abcde';
+// alert(a[0]);
+// alert(a[2]);
+// alert(a[4]);
+// let a = "abcde";
+// let b = a[4] + a[3] + a[2] + a[1] + a[0];
+// alert(b);
+// let str = 'abcde';
+// let num = 0;
+// alert(str[num]);
+// let str = 'abcde';
+// let last = str.length - 3;
+// alert(str[last]);
+// let a = '12345';
+// alert(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]));
+// let a = String(12345);
+// alert(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]));
+// let a = String(12345);
+// let b = a[4] + a[3] + a[2] + a[1] + a[0];
+// alert(b);
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+// let a = 0.1 * 0.2;
+// alert(+a.toFixed(2));
+// let a = 0.3 - 0.1;
+// alert(+a.toFixed(2));
+// alert('Твой возраст: ' + prompt('Сколько тебе лет?'));
+// let num1 = prompt('Введите длину стороны квадрата:');
+// alert(Number(num1**2));
+// let num1 = prompt('Введите первую сторону прямоугольника');
+// let num2 = prompt('Введите вторую сторону прямоугольнка');
+// alert((num1*2) + (num2*2));
+// document.write('<i>AcademyKings</i.>')
+// let str = 'text';
+// document.write('<i>text</i>')
+// document.write('<ol>1</ol>')
+// document.write('<ol>2</ol>')
+// document.write('<ol>3</ol>')
+// document.write('<ol>4</ol>')
+// document.write('<ol>5</ol>')
+// let a = 24 * 60;
+// alert(a * 60);
+// let a = 30 * 24;
+// let b = a * 60;
+// alert(b * 60);
+// let a = 365 * 24;
+// let b = a * 60;
+// alert(b * 60);
+// alert(2**20);
+// alert(2**30);
+// let a = 2 ** 30;
+// alert(a * 10);
+// alert(2**40);
+
+
+
+
